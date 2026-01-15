2b982a851eb709263aa6ebea684c083e7dc5bdc8
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2026-01-16 01:11:19 +0530

    Update

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,17 @@
 import axios from "axios";
+import { getDeviceId } from "../utils/deviceId";
 
 const API = axios.create({
     baseURL: import.meta.env.VITE_APP_API,
     withCredentials: true
 });
+
+API.interceptors.request.use(
+    (config) => {
+        config.headers["x-device-id"] = getDeviceId();
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 export default API;
